9990607de91149cd26eeb04f82593973ea0f6c70
vian14 <44376121+vian14@users.noreply.github.com>
2019-08-25 00:22:18 +0700

    Update vote.js

--- cmd/vote.js
+++ cmd/vote.js
@@ -5,11 +5,11 @@ const disagree = "❎";
 
 exports.run = async (bot, msg, params) => {
 
-  let VOTE_TEXT = await msg.channel.send("Vote now! (10 Seconds)");
+  let VOTE_TEXT = await msg.channel.send("Vote now! (30 minutes)");
   await VOTE_TEXT.react(agree);
   await VOTE_TEXT.react(disagree);
 
-  const reactions = await VOTE_TEXT.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 10000});
+  const reactions = await VOTE_TEXT.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 1800000});
   VOTE_TEXT.delete();
 
   var NO_Count = reactions.get(disagree).count;
